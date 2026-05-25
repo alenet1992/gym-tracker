5055badf6747d79111ee499b9f4c55d639043c36
tiago.pereira <alexandre.tiago@hotmail.com>
2026-05-25 10:53:36 +0100

    teste

--- api/index.ts
+++
@@ -1,8 +0,0 @@
-import 'reflect-metadata';
-import { createApp } from '../dist/app.factory';
-
-export default async function handler(req: unknown, res: unknown) {
-  const app = await createApp();
-  const instance = app.getHttpAdapter().getInstance();
-  return instance(req, res);
-}
